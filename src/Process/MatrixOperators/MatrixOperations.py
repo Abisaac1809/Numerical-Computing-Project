13cf4e11e0d9020bb618eee0f2dc431080512596
Ricardo <alzuruttricardo@gmail.com>
2025-06-22 21:56:05 -0400

    feat: MatrixOperations subtraction is implemented

--- src/Process/MatrixOperators/MatrixOperations.py
+++ src/Process/MatrixOperators/MatrixOperations.py
@@ -14,3 +14,14 @@ class MatrixOperations:
             for j in range(matrix_a.shape[1]):
                 result[i, j] = matrix_a[i, j] + matrix_b[i, j]
         return result
+    
+    def subtract(self, matrix_a, matrix_b):
+        """Resta dos matrices usando bucles for"""
+        if not self.validator.can_add_or_subtract(matrix_a, matrix_b):
+            raise ValueError("Las matrices no tienen las mismas dimensiones")
+        
+        result = np.zeros((matrix_a.shape[0], matrix_a.shape[1]))
+        for i in range(matrix_a.shape[0]):
+            for j in range(matrix_a.shape[1]):
+                result[i, j] = matrix_a[i, j] - matrix_b[i, j]
+        return result
