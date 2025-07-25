63c9251b66425287709a2c402c868830c3bd8d9c
Ricardo Alzurutt <95257402+raagito@users.noreply.github.com>
2025-07-24 20:53:17 -0400

    [Feature] implement traffic control and refine UI (#32)
    * feat: Creation of the HTML template with the graph and data
    
    * feat: Modify the method that handles the request to render the HTML template
    
    * feat: Configure the URL to be visited a maximum of 5 times per minute from a device; if exceeded, display an alert
    
    * feat: In the function that handles the request, once the dictionary with the list of points and the URL is available, use this data to create the results file with set notation

--- Apps/DataVisualization/views.py
+++ Apps/DataVisualization/views.py
@@ -7,12 +7,35 @@ from Apps.NumericalMethods.Solvers.MatrixOperators.SystemOfEquationsSolver impor
 from Apps.Common.Repositories.DataModels.Point import Point
 from Apps.DataVisualization.Methods.GraphVisualizer import GraphVisualizer
 import numpy as np
+from Apps.Common.Composables.MathReport import MathReport
+
 from .Methods.RandomGraphUtils import *
 
-def generateRandomGraphAndPoints(request):
+last_access_times = {}
+
+def data_visualization_view(request):
+    client_ip = request.META.get('REMOTE_ADDR')
+    current_time = datetime.now()
+
+    # Rate limiting logic
+    if client_ip not in last_access_times:
+        last_access_times[client_ip] = []
+    
+    # Remove access times older than 1 minute
+    last_access_times[client_ip] = [
+        t for t in last_access_times[client_ip] if current_time - t < timedelta(minutes=1)
+    ]
+
+    if len(last_access_times[client_ip]) >= 5:
+        alert_message = "Access limit exceeded: You can only visit this page 5 times per minute."
+        return render(request, 'DataVisualization/data_visualization.html', {'alert_message': alert_message})
+    
+    last_access_times[client_ip].append(current_time)
+    #------------------------------------------------------------------------
     generator = archiveGenerator()
     equationSolver = MatrixEquationSolver()
     gaussSolver = SystemOfEquationsSolver()
+    report = MathReport("MathReport.txt")
     variableNames = ['A', 'B', 'C']
 
     matrixFiles: list[str] = generateMatrixFiles(generator, 3)
@@ -25,5 +48,11 @@ def generateRandomGraphAndPoints(request):
     points: list[Point] = solvePoints(gaussSolver, equationResults, variableNames)
     setPointsGroup(points)
     image: str = GraphVisualizer.plotPointsAndDistances3D(points)
-    print(image)
-    return JsonResponse({'image': image, 'points': [point.toDict() for point in points]})
+   
+    report.writeOriginalMatrices(matrixs)
+    report.writeFormulasAndResults(formulas, equationResults)
+    report.writeSystemsAndSolutions(equationResults, points)
+    report.writeDistancesBetweenPoints(points)
+    return render(request, 'index.html')
+
+
